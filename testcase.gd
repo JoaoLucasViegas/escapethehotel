73e917bd3ca3a4346ce61a6a252acd7471d6bebd
João Lucas S. Viegas <64292055+JoaoLucasViegas@users.noreply.github.com>
2025-06-23 22:41:01 -0300

    Create testcase

---
+++ testcase.gd
@@ -0,0 +1,37 @@
+______
+Title:
+Switching off - The Haunted Hotel
+
+________
+Concept:
+Turn the power off to escape from the haunted hotel.
+
+_________
+Paradigm:
+You gotta make your way to find the exit out of this hotel without getting caught.
+
+_________
+Features:
+>> Hidding
+>> Obstacles
+>> Sabotage
+
+__________
+Mechanics:
+>> Hidding
+	Hide under beds and cabinets to avoid getting caught, when they enter a room you're in.
+>> Sabotage
+	Turning the lights off will make your way out of the situation easier, gaining the advantage over enemies, also makes you invisible to their sights but not to their "touch" (you can touch them and still get caught).
+	They might eventually going to fix to be able to see.
+	It might be eventually more than just "lights" but for now take lights off as sabotage.
+>> Obstacles
+	Sometimes the environment won't be on your side so you will need to make your way out by "puzzling" things, maybe sabotaging stuff to get some enemy attention or by blocking their way in some way.
+	It might as well be a safety tool to avoid getting easily caught when chased.
+
+________________
+Game Jam extras:
+Theme - "Unseen"
+Extra points
+	>> Immersive - by having your UI being part of the world.
+	>> Boom - by making at least one explosion in the game.
+	>> Report card - by grading something the player done.
